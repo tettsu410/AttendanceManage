bbf18fa0176b76fd5b35edf1ed6871cda4a72d1d
Honda467 <taka42614@gmail.com>
2023-11-21 17:57:09 +0900

    AdministratorController.java　UserService.java User.java 追加＆作成

---
+++ src/main/java/com/example/AttendanceManage/AdministratorController.java
@@ -0,0 +1,38 @@
+package com.example.AttendanceManage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AdministratorController {
+
+    @GetMapping("/admin")
+    public String adminHome(Model model) {
+        // ダミーデータを使用している例
+        List<User> userList = UserService.getAllUsers();
+        model.addAttribute("users", userList);
+        return "admin";
+    }
+
+    @GetMapping("/admin/addUser")
+    public String showAddUserForm() {
+        return "addUser";
+    }
+
+    @PostMapping("/admin/addUser")
+    public String addUser(User user) {
+        // ユーザーの追加処理（データベースへの保存など）を行う
+        UserService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/logout")
+    public String logout() {
+        // ログアウト処理（セッションの無効化など）
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/AttendanceManage/User.java
@@ -0,0 +1,8 @@
+package com.example.AttendanceManage;
+public class User {
+    private String fullName;
+    private String username;
+    private String email;
+    private String attendance;
+
+}
