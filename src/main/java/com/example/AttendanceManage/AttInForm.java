56311186fc7861b4fa8795f3f4b3dafb788c1fd6
tettsu410 <tettsu.410@gmail.com>
2023-12-06 11:30:28 +0900

    2023/12/6

---
+++ src/main/java/com/example/AttendanceManage/AttInForm.java
@@ -0,0 +1,14 @@
+package com.example.AttendanceManage;
+
+public class AttInForm {
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    private int userId;
+
+}
