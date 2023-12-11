d59e412bee6b387f8391298a8d5fd68b15bef786
OkamotoKaito <hl.kobe209196286@gmail.com>
2023-12-11 10:24:22 +0900

    中間

--- src/main/java/com/example/AttendanceManage/Attendance.java
+++ src/main/java/com/example/AttendanceManage/Attendance.java
@@ -1,12 +1,16 @@
 package com.example.AttendanceManage;
 
 import java.sql.Time;
+import java.util.Date;
 
 public class Attendance {
     private int id;
+    private Date date;
     private Time start_time;
-
     private Time end_time;
+    private Time break_start;
+    private Time break_end;
+    private Time total_time;
 
     public int getId() {
         return id;
@@ -16,11 +20,19 @@ public class Attendance {
         this.id = id;
     }
 
-    public Time getBegin_time() {
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getStart_time() {
         return start_time;
     }
 
-    public void setBegin_time(Time begin_time) {
+    public void setStart_time(Time start_time) {
         this.start_time = start_time;
     }
 
@@ -30,5 +42,21 @@ public class Attendance {
 
     public void setEnd_time(Time end_time) {
         this.end_time = end_time;
+    }
+
+    public Time getBreak_start() {
+        return break_start;
+    }
+
+    public void setBreak_start(Time break_start) {
+        this.break_start = break_start;
+    }
+
+    public Time getBreak_end() {
+        return break_end;
+    }
+
+    public void setBreak_end(Time break_end) {
+        this.break_end = break_end;
     }
 }
