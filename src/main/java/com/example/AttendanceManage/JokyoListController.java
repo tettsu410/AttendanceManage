0aad789aacf611aaf7b1a785bda0b442636e0b9b
tettsu410 <tettsu.410@gmail.com>
2024-01-16 16:32:22 +0900

    2024/1/16-1

--- src/main/java/com/example/AttendanceManage/JokyoListController.java
+++ src/main/java/com/example/AttendanceManage/JokyoListController.java
@@ -1,6 +1,8 @@
 package com.example.AttendanceManage;
 
 import java.sql.Time;
+
+import ch.qos.logback.core.LayoutBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,13 +23,17 @@ public class JokyoListController {
     public String attendances(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         MyUserDetails user = (MyUserDetails) authentication.getPrincipal();
+        System.out.println(user.getId());
+        System.out.println(user.getUsername());
 
         String sql = "SELECT id,date,start_time,end_time,break_start,break_end FROM ATTENDANCES where id = ?";
+
         List<Attendance> attendances = jdbcTemplate.query(sql,new DataClassRowMapper<>(Attendance.class),user.getId());
         model.addAttribute("attendances",attendances);
         return "jokyoList";
     }
 }
+
 
 //import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
